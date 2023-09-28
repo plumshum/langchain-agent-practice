c8ae56d969801ef4cab4edb2e4ebad53f970e912
plumshum <91917682+plumshum@users.noreply.github.com>
2023-09-28 03:15:59 +0000

    uploaded from replit

--- main.py
+++ main.py
@@ -10,4 +10,5 @@ llm = OpenAI(temperature = 0)
 tools = load_tools(["wikipedia", "llm-math"], llm = llm)
 agent = initialize_agent(tools, llm, agent = 'zero-shot-react-description', verbose = True)
 
-agent.run("In what year was the film Departed with Leonardo Dicaprio released? What is the year raised to the 0.43 power?")
+input = ("Ask a question, any question :)")
+agent.run(input)
